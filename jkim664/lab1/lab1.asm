277ff344a0d192c13461aa4e47a835be40386755
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-08-28 19:54:37 +0000

    finished lab1

--- jkim664/lab1/lab1.asm
+++ jkim664/lab1/lab1.asm
@@ -6,11 +6,64 @@
  ;            print the character to the monitor
 
  .ORIG x3000
+	AND R1, R1, #0	
+	AND R2, R2, #0
+	AND R4, R4, #0	
+	AND R0, R0, #0	
+	ADD R1, R1, #4  ;character counter
+	
+
+Q	BRz END 
+	ADD R2, R2, #4
+START	AND R3, R3, R3	;set cc	
+	BRn ADD1
+	BRzp ADD0
+
+ADD1	ADD R4, R4, R4 
+	ADD R4, R4, #1	;add 1
+	BRnzp SHIFT	
+
+ADD0 	ADD R4, R4, R4	;left shift	
+	BRnzp SHIFT
+
+SHIFT 	ADD R3, R3, R3 ;shift R3 to the left
+	ADD R2, R2, #-1 ;subtract from bit counter
+	BRp START 
+	BRnz CHECK 
+
+CHECK	ADD R4, R4, #-10
+	BRn DIGIT
+	BRzp LETTER
+
+DIGIT	ADD R4, R4, #10
+     	ADD R4, R4, #10 	
+	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #8
+	BRnzp PRINT
+
+LETTER	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #10
+	ADD R4, R4, #5
+	BRnzp PRINT
+
+PRINT 	ADD R0, R4, #0 
+	OUT
+	AND R4, R4, #0	
+	ADD R1, R1, #-1 
+	BRnzp Q
+	
+
 
 
 
  ; stop the computer
- HALT
+ END	HALT
 
  ; program data section starts here
 
