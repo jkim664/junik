364d73354c475e17460d5e65b82039d5de5f9b89
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-08-28 20:00:50 +0000

    added comments

--- jkim664/lab1/lab1.asm
+++ jkim664/lab1/lab1.asm
@@ -6,20 +6,20 @@
  ;            print the character to the monitor
 
  .ORIG x3000
-	AND R1, R1, #0	
+	AND R1, R1, #0	;clearing registers 
 	AND R2, R2, #0
-	AND R4, R4, #0	
+	AND R4, R4, #0	;where the bits will be stored from R3
 	AND R0, R0, #0	
 	ADD R1, R1, #4  ;character counter
 	
 
 Q	BRz END 
-	ADD R2, R2, #4
+	ADD R2, R2, #4	;bit counter
 START	AND R3, R3, R3	;set cc	
 	BRn ADD1
 	BRzp ADD0
 
-ADD1	ADD R4, R4, R4 
+ADD1	ADD R4, R4, R4 	;left shift
 	ADD R4, R4, #1	;add 1
 	BRnzp SHIFT	
 
@@ -31,11 +31,11 @@ SHIFT 	ADD R3, R3, R3 ;shift R3 to the left
 	BRp START 
 	BRnz CHECK 
 
-CHECK	ADD R4, R4, #-10
+CHECK	ADD R4, R4, #-10 ;check if it's digit or letter
 	BRn DIGIT
 	BRzp LETTER
 
-DIGIT	ADD R4, R4, #10
+DIGIT	ADD R4, R4, #10 ;add to find ascii 
      	ADD R4, R4, #10 	
 	ADD R4, R4, #10
 	ADD R4, R4, #10
@@ -43,7 +43,7 @@ DIGIT	ADD R4, R4, #10
 	ADD R4, R4, #8
 	BRnzp PRINT
 
-LETTER	ADD R4, R4, #10
+LETTER	ADD R4, R4, #10 ;add to find ascii
 	ADD R4, R4, #10
 	ADD R4, R4, #10
 	ADD R4, R4, #10
@@ -52,10 +52,10 @@ LETTER	ADD R4, R4, #10
 	ADD R4, R4, #5
 	BRnzp PRINT
 
-PRINT 	ADD R0, R4, #0 
+PRINT 	ADD R0, R4, #0 ;load to R0 to TRAP out
 	OUT
-	AND R4, R4, #0	
-	ADD R1, R1, #-1 
+	AND R4, R4, #0 ;Clear bit holder 
+	ADD R1, R1, #-1 ;subtract from character counter
 	BRnzp Q
 	
 
