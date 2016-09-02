69837932676838ef394694ff31b666eab41db7ac
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-09-02 23:38:07 +0000

    finished lab2a

--- jkim664/lab2a/lab2a.asm
+++ jkim664/lab2a/lab2a.asm
@@ -2,7 +2,20 @@
 ;read comments
 ;
 .ORIG x3000
-		
+START	AND R0, R0, #0
+	GETC
+	OUT
+	ADD R0, R0, #-10
+	BRz NLINE
+	ADD R0, R0, #-3
+	BRz NLINE
+	ADD R0, R0, #-7
+	ADD R0, R0, #-12
+	BRz START
+	JSR IS_BALANCED
+	
+
+
 SPACE	.FILL x0020
 NEW_LINE	.FILL x000A
 CHAR_RETURN	.FILL x000D
@@ -11,7 +24,45 @@ CHAR_RETURN	.FILL x000D
 ;input - R0 holds the input
 ;output - R5 set to -1 if unbalanced. else not modified.
 IS_BALANCED
+	ADD R0, R0, #-8
+	BRz LEFT
+	BRp RIGHT
 	
+LEFT	ADD R0, R0, #10
+	ADD R0, R0, #15
+	ADD R0, R0, #15
+	JSR PUSH 
+	BRnzp START
+
+RIGHT	ADD R0, R0, #10
+	ADD R0, R0, #15
+	ADD R0, R0, #15
+	JSR POP
+	LD R1, NEG_OPEN
+	ADD R1, R1, R0	
+	BRz START
+	ADD R5, R5, #-2
+	BRp EXIT 	
+	
+
+NLINE	ADD R5, R5, #0
+	BRp DONE
+	BRnz EXIT
+	
+	
+
+EXIT	JSR POP
+	LD R1, NEG_OPEN
+	ADD R1, R1, R0	
+	BRnp DONE
+	ADD R5, R5, #-1
+	
+
+DONE	HALT	
+
+
+
+
 NEG_OPEN .FILL xFFD8
 ;IN:R0, OUT:R5 (0-success, 1-fail/overflow)
 ;R3: STACK_END R4: STACK_TOP
@@ -21,7 +72,7 @@ PUSH
 	ST R4, PUSH_SaveR4	;save R4
 	AND R5, R5, #0		;
 	LD R3, STACK_END	;
-	LD R4, STACk_TOP	;
+	LD R4, STACK_TOP	;
 	ADD R3, R3, #-1		;
 	NOT R3, R3		;
 	ADD R3, R3, #1		;
@@ -36,7 +87,7 @@ OVERFLOW
 DONE_PUSH
 	LD R3, PUSH_SaveR3	;
 	LD R4, PUSH_SaveR4	;
-	RET
+	RET 
 
 
 PUSH_SaveR3	.BLKW #1	;
@@ -55,13 +106,15 @@ POP
         NOT R3, R3              ;
         ADD R3, R3, #1          ;
         ADD R3, R3, R4          ;
-        BRz UNDERFLOW           ;
+        BRz UNDERFLOW	        ;
         ADD R4, R4, #1          ;
         LDR R0, R4, #0          ;
         ST R4, STACK_TOP        ;
         BRnzp DONE_POP          ;
 UNDERFLOW
         ADD R5, R5, #1          ;
+	BRnzp DONE_POP
+				;
 DONE_POP
         LD R3, POP_SaveR3       ;
         LD R4, POP_SaveR4       ;
