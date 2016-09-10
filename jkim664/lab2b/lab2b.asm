16022751eceb27537fc8596ca3e6d88365d707b5
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-09-10 07:23:14 +0000

    lab2b finished

--- jkim664/lab2b/lab2b.asm
+++ jkim664/lab2b/lab2b.asm
@@ -2,16 +2,16 @@
 ;it will be positive.
 .ORIG x3000
 
-
-
-
-
-
-
-
-
-
-
+	AND R0, R0, #0
+	AND R1, R1, #0 
+	AND R2, R2, #0
+	AND R4, R4, #0 ; quotient counter
+	AND R5, R5, #0
+	AND R6, R6, #0
+	ADD R1, R1, #-10 ; value of -10
+	ADD R2, R2, #10 ; value of 10
+	LD R6, ASCII_0	; value of the ascii value 0
+	JSR DIV ; jump to division subroutine
 
 
 ASCII_0 .FILL x30
@@ -19,7 +19,33 @@ ASCII_0 .FILL x30
 ;input R3, R4
 ;out R0-quotient, R1-remainder
 DIV	
-	RET
+	ADD R3, R3, R1 ; subtract 10 
+	BRn REMAINDER ; if negative, go to REMAINDER
+	ADD R4, R4, #1 ; increment quotient counter
+	BRnzp DIV
+
+
+REMAINDER
+	ADD R0, R3, R2 ; add 10 to restore value of R3 and load into R0
+	JSR PUSH 
+	AND R0, R0, #0 ; clear R0
+	AND R3, R3, #0 ; clear R3
+	ADD R3, R4, #0 ; Transfer quotient from R4 to R3
+	BRz POPPIN ; if quotient is 0, start popping values
+	AND R4, R4, #0 ; clear R4
+	BRnzp DIV
+
+POPPIN
+	JSR POP 
+	ADD R5, R5, #0
+	BRp STOP ; check if underflow
+	ADD R0, R0, R6 ; add popped value with ascii value of 0
+	OUT 
+	BRnzp POPPIN
+
+
+
+STOP	HALT	
 
 
 ;IN:R0, OUT:R5 (0-success, 1-fail/overflow)
