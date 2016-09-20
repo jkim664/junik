8393cd286d082c524e2862ffa8a7c0c3cd9cdebc
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-09-20 04:17:02 +0000

    finished mp3

--- jkim664/mp3/main.c
+++ jkim664/mp3/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#define PI 3.14159
 int main()
 {
 	double omega1, omega2, omegac, T, dt;
@@ -18,7 +18,25 @@ int main()
 	double Voutnew = 0, Voutcur = 0, Voutprev = 0;
 
 	//Write your code here!
+	//Intro - this code implements the differential equation in C. It takes 
+	//values Voutnew, Voutcur and Voutprev and manipulates those values to figuree
+	//out the 2nd order low pass filter using the finite difference method.
+	for(i = 0; i < N; i++)
+	{
 
+	Voutprev = Voutcur;	//put value of current to previous one 
+	Voutcur = Voutnew;	//put value of new to current one 
+	printf("%lf\n", Voutnew);
+			
+	double comp1 = 1/(sqrt(2)*dt*omegac); 
+	double comp2 = 1/(pow(dt*omegac,2));
+	double comp3 = 1/(comp1 + comp2);
+	double comp4 = ((2/(pow(dt*omegac,2))) - 1)*Voutcur;
+	double comp5 = (comp1 - comp2)*Voutprev;
+	double comp6 = sin(omega1*i*dt) + .5*sin(omega2*i*dt);
 
+	Voutnew = comp3*(comp4 + comp5 + comp6); 
+
+	}
 	return 0;
 }
