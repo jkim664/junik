c41b466df6fb66985e6b84d44fb8c0968c49722c
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-02 19:41:25 +0000

    finished lab5

--- jkim664/Lab5/main.c
+++ jkim664/Lab5/main.c
@@ -8,4 +8,27 @@
  * Otherwise print "\n"
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "dice.h"
 
+int main()
+{
+	int one, two, three, seed;
+	printf("enter a seed number.\n");
+	scanf("%d", &seed);
+	srand(seed); 
+	roll_three(&one, &two, &three);
+	printf("%d %d %d\n", one, two, three);
+	
+	if(one == two && two == three)
+	printf("triple!\n");
+	else if(one == two)
+	printf("double!\n");
+	else if(one == three)
+	printf("double!\n");
+	else if(two == three)
+	printf("double!\n");
+
+return 0;
+}
