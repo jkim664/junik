fc6c8f7e52b19fc2212f07bd23486bf0c4848aa1
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-02 19:41:25 +0000

    finished lab5

--- Lab5/dice.c
+++ Lab5/dice.c
@@ -1,5 +1,12 @@
 //Function for generating three d6 rolls
+#include <stdlib.h>
+#include "dice.h"
 
 void roll_three(int* one, int* two, int* three){
+
+
+*one = rand() % 6 + 1;
+*two = rand() % 6 + 1;
+*three = rand() % 6 + 1;
 
 }
