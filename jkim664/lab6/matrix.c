fbc874cf8c59fafb9d388d5e748111b27b02106e
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-08 18:02:34 +0000

    finished lab6

--- jkim664/lab6/matrix.c
+++ jkim664/lab6/matrix.c
@@ -6,8 +6,26 @@
  *        matrixB, a one dimensional double array of size k*n
  * OUTPUT: matrixC, a one dimensional double array of size m*n
  */
+#include "matrix.h"
 void matrix_multiply(double *matrixA,double *matrixB,double *matrixC,int m,int k,int n)
 {
 //YOUR CODE HERE
-}
+	int i, j, l;
+		for(i = 0; i < k; i++)
+		{
+			for(j = 0; j < n ; j++)
+			{
+				double curr = 0, new = 0;
+				for(l = 0; l < k ; l++)			
+				{	
+					curr = new;
+					new = matrixA[i*k + l] * matrixB[l*n + j] + curr;
+				}
+		
+			matrixC[i*n + j] = new;				
+
+			}
+
+		}	
+}		
 
