916be7c24f1d4c03a4bf190c0c222e82cf16517b
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-21 16:37:22 +0000

    finished lab8

--- jkim664/lab8/sliding.c
+++ jkim664/lab8/sliding.c
@@ -3,5 +3,28 @@
 */
 void slide_up(int* my_array, int rows, int cols){
 
+int i, j, c;
+
+	for(j = 0; j < cols; j++)
+	{
+		for(i = 0; i < rows ; i++)
+		{
+			if(my_array[i * cols + j] != -1)
+			{		
+				for(c = 0; c < i; c++)
+				{
+					if(my_array[c * cols + j] == -1)
+					{
+						my_array[c * cols + j] = my_array[i * cols + j];
+						my_array[i * cols + j] = -1;
+					}	
+				}	
+			}
+		}
+	}
+
+
+
+
     return;
 }
