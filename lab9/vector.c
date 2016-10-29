47ac12ba7c10e3f0407af27f9361d137ea40502a
jkim664 <jkim664@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-29 00:13:47 +0000

    finished lab9

--- lab9/vector.c
+++ lab9/vector.c
@@ -4,25 +4,59 @@
 
 vector_t * createVector(int initialSize)
 {
+	vector_t * vector = malloc(sizeof(vector_t));
+	vector->maxSize = initialSize;
+
+	vector->size = 0;
+	vector->array = (int *) malloc(vector->maxSize);
+
+	return vector;
 }
 
 void destroyVector(vector_t * vector)
 {
+	free(vector->array);	
+	free(vector);
 }
 
 void resize(vector_t * vector)
 {
+	vector->maxSize = vector->maxSize * 2;
+	vector->array = (int *) realloc(vector->array, vector->maxSize); 
 }
 
 void push_back(vector_t * vector, int element)
 {
+	if(vector->size >= vector->maxSize)
+	resize(vector);
+	
+	vector->array[vector->size] = element;
+	
+	vector->size = vector->size + 1;
+
 }
 
 int pop_back(vector_t * vector)
 {
+	int value;
+	value = vector->array[vector->size - 1];
+
+	vector->size--;
+
+	return value;
+
+
 }
 
 int access(vector_t * vector, int index)
 {
+	if(index > vector->size)
+	return 0;
+
+	else
+	return vector->array[index];
+
+	
+
 }
 
